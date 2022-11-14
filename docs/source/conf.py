2cc39ff6e22640afb8ca73bbdfbfe994d318ac54
Fynn Aschmoneit <fynnja@math.aau.dk>
2022-11-14 16:14:01 +0100

    updating documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,12 @@ author = 'Fynn Aschmoneit'
 release = '0.1'
 version = '0.1.0'
 
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src/MultiphaseTestBench'))
+
+
 # -- General configuration
 
 extensions = [
