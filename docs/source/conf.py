7160d6c785c79c50b6db1071ecfbc5fcdfd4c114
Fynn Aschmoneit <fynnja@math.aau.dk>
2022-11-14 16:53:21 +0100

    updating documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas']
+MOCK_MODULES = ['numpy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
